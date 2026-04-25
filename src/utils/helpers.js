76cf2967acad7cf43210a5cf73a7c7420e2942b4
LibraAkaja <adhikaribiraj908@gmail.com>
2026-04-25 13:35:05 +0545

    Helpers to get random options

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,4 +3,12 @@ const getRandomOptions = (list, count = 4) => {
     return shuffled.slice(0, count);
 };
 
+const types = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy'];
+
+const getRandomTypes = (count = 4) => {
+    const shuffled = [...types].sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, count);
+};
+
 export default getRandomOptions;
+export { getRandomTypes };

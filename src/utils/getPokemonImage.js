7e4c17f8fadb5896f59321e04f4a24beb457700d
LibraAkaja <adhikaribiraj908@gmail.com>
2026-04-25 13:34:24 +0545

    Helper to fetch pokemon images

--- src/utils/getPokemonImage.js
+++ src/utils/getPokemonImage.js
@@ -1,7 +1,31 @@
+// Cache for loaded images to avoid re-fetching
+const imageCache = new Map();
+
 const getPokemonImage = (id) => {
-    const dream = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+    if (imageCache.has(id)) {
+        return imageCache.get(id);
+    }
+
     const official = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
-    return { dream, official };
+    const dream = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`;
+
+    const images = { official, dream };
+
+    imageCache.set(id, images);
+
+    return images;
+};
+
+export const preloadPokemonImage = (id) => {
+    const { official, dream } = getPokemonImage(id);
+
+    // Preload official artwork first
+    const img = new Image();
+    img.src = official;
+
+    // Preload dream world as fallback
+    const fallbackImg = new Image();
+    fallbackImg.src = dream;
 };
 
 export default getPokemonImage;

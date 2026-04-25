4096427e003ffbed34d9d08f0bdfeaabfd31030f
LibraAkaja <adhikaribiraj908@gmail.com>
2026-04-25 13:40:58 +0545

    Import and use game banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import './App.css'
 import Game from './components/Game.jsx';
+import GameLogo from './assets/PokeGame.png';
 
 function App() {
 
   return (
     <>
+      <img src={GameLogo} className='game-logo'/>
       <Game/>
     </>
   )

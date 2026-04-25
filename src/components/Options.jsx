e8e71d377f27e3712a187c600ac02b662fb91c39
LibraAkaja <adhikaribiraj908@gmail.com>
2026-04-25 13:35:55 +0545

    Component for options

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,9 +1,16 @@
 const Options = ({options = [], onGuess, disabled}) => {
+    const capitalizeFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+    
     return(
-        <div style={{display:"flex", gap:"10px", justifyContent:"center"}}>
+        <div className="options-container">
             {options.map((opt, i) => (
-                <button key={i} onClick={() => onGuess(opt)} disabled={disabled}>
-                    {opt.name}
+                <button 
+                    key={i} 
+                    className="option-button"
+                    onClick={() => onGuess(opt)} 
+                    disabled={disabled}
+                >
+                    {typeof opt === 'string' ? capitalizeFirst(opt) : capitalizeFirst(opt.name)}
                 </button>
             ))}
         </div>

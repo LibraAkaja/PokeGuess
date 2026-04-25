63bdbc3c405ee788b0d84b877b0c72812b6744f4
LibraAkaja <adhikaribiraj908@gmail.com>
2026-04-25 13:31:34 +0545

    Lives board for game

--- src/components/Lives.jsx
+++ src/components/Lives.jsx
@@ -1,6 +1,6 @@
 const Lives = ({ lives }) => {
     return (
-        <div className="lives">
+        <div className="lives-container">
             {Array.from({ length: lives }).map((_, i) => (
                 <span key={i} className="heart">❤️</span>
             ))}

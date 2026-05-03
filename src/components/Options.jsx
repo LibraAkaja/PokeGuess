beadd2364a8312fd6d18f189566cd51565c42bfa
LibraAkaja <adhikaribiraj908@gmail.com>
2026-05-03 13:07:21 +0545

    Add a function to play audio

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,3 +1,11 @@
+import mouseHoverAudio from "../assets/Hover-Btn.WAV";
+import mouseClickAudio from "../assets/Mouse-Click.WAV";
+
+export const playAudio = (a) => {
+    const aud = new Audio(a);
+    aud.play();
+};
+
 const Options = ({options = [], onGuess, disabled}) => {
     const capitalizeFirst = (str) => str.charAt(0).toUpperCase() + str.slice(1);
     
@@ -7,7 +15,8 @@ const Options = ({options = [], onGuess, disabled}) => {
                 <button 
                     key={i} 
                     className="option-button"
-                    onClick={() => onGuess(opt)} 
+                    onClick={() => {playAudio(mouseClickAudio); onGuess(opt);}}
+                    onMouseOver={() => playAudio(mouseHoverAudio)} 
                     disabled={disabled}
                 >
                     {typeof opt === 'string' ? capitalizeFirst(opt) : (opt?.name ? capitalizeFirst(opt.name) : 'Unknown')}
